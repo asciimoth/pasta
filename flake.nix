ed6110654ead15dd800b586b015989a3509e19e4
AsciiMoth <ascii@moth.contact>
2026-05-11 18:01:06 +0400

    chore: init project

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "pasta dev env";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            commitizen.enable = true;
+            typos.enable = true;
+            typos-commit = {
+              enable = true;
+              description = "Find typos in commit message";
+              entry = let script = pkgs.writeShellScript "typos-commit" ''
+                typos "$1"
+              ''; in builtins.toString script;
+              stages = [ "commit-msg" ];
+            };
+            govet.enable = true;
+            gofmt.enable = true;
+            golangci-lint.enable = true;
+            gotidy = {
+              enable = true;
+              description = "Makes sure go.mod matches the source code";
+              entry = let script = pkgs.writeShellScript "gotidyhook" ''
+                go -C configer mod tidy -v
+                go -C hujson mod tidy -v
+                go work sync
+              ''; in builtins.toString script;
+              stages = [ "pre-commit" ];
+            };
+            golangtest = {
+              enable = true;
+              description = "go test ./configer/... ./hujson/... --race";
+              entry = let script = pkgs.writeShellScript "gotidyhook" ''
+                go test ./configer/... ./hujson/... --race
+              ''; in builtins.toString script;
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          go
+          golangci-lint
+          gopls
+
+          typos
+          commitizen
+
+          just
+        ];
+      };
+    });
+}
---
+++ pasta/go.mod
@@ -0,0 +1,3 @@
+module github.com/asciimoth/pasta/pasta
+
+go 1.25.5
