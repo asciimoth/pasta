f35c7c8fbd101c436075fa1364c88a0c84eb2023
AsciiMoth <ascii@moth.contact>
2026-05-11 18:06:33 +0400

    feat: add logger interface

---
+++ pasta/logger.go
@@ -0,0 +1,15 @@
+package pasta
+
+// Implementations must be safe for concurrent use.
+type Logger interface {
+	Debug(args ...any)
+	Debugf(format string, args ...any)
+	Info(args ...any)
+	Infof(format string, args ...any)
+	Warn(args ...any)
+	Warnf(format string, args ...any)
+	Err(args ...any)
+	Errf(format string, args ...any)
+	Fatal(args ...any)
+	Fatalf(format string, args ...any)
+}
